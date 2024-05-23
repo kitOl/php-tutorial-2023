689f17e76a6eb12b94386c261bfe4b29c01681b0
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-05-23 16:08:21 +0300

    11 view some any array functions

---
+++ 11/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/main.css">
+  <title>PHP Tutorial 2023 :: Arrays</title>
+</head>
+
+<body>
+  <pre>
+  <?php
+  $fruits = [
+    'Apple',
+    'Banana',
+    'Cherry'
+  ];
+  print_r($fruits);
+
+  array_push($fruits, 'kiwi');
+  print_r($fruits);
+
+  echo array_pop($fruits), '<br>';
+  print_r($fruits);
+
+  print_r(array_reverse($fruits));
+  print_r($fruits);
+
+  $array2 = ['kiwi', 'duryan'];
+  print_r(array_merge($fruits, $array2));
+
+
+
+  ?>
+</pre>
+</body>
+
+</html>
