ef2827ca4784f9e17261a014175f3ecb7c0d44db
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-05-26 07:21:11 +0300

    26 add session security

---
+++ 26/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/main.css">
+  <title>PHP Tutorial 2023 :: SESSION SECURITY</title>
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
